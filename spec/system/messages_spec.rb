1d0f67c92a14c4d20ea17c4870fa610ce23f5268
gaki-h <walter.thi.gks@gmail.com>
2022-01-12 18:03:15 +0900

    画像の投稿

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -48,6 +48,7 @@ RSpec.describe "メッセージ投稿機能", type: :system do
       expect(page).to have_content(post)
     end
   end
+
     it '画像の投稿に成功すると、投稿一覧に遷移して、投稿した画像が表示されている' do
       # サインインする
       sign_in(@room_user.user)
@@ -59,15 +60,20 @@ RSpec.describe "メッセージ投稿機能", type: :system do
       image_path = Rails.root.join('public/images/test_image.png')
 
       # 画像選択フォームに画像を添付する
+      attach_file('message[image]', image_path, make_visible: true)
 
       # 送信した値がDBに保存されていることを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change { Message.count }.by(1)
 
       # 投稿一覧画面に遷移していることを確認する
+      expect(current_path).to eq(room_messages_path(@room_user.room))
 
       # 送信した画像がブラウザに表示されていることを確認する
-
+      expect(page).to have_selector('img')
     end
-
+  
     it 'テキストと画像の投稿に成功する' do
       # サインインする
       sign_in(@room_user.user)
@@ -79,14 +85,21 @@ RSpec.describe "メッセージ投稿機能", type: :system do
       image_path = Rails.root.join('public/images/test_image.png')
 
       # 画像選択フォームに画像を添付する
+      attach_file('message[image]', image_path, make_visible: true)
 
       # 値をテキストフォームに入力する
+      post = 'テスト'
+      fill_in 'message_content', with: post
 
       # 送信した値がDBに保存されていることを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change { Message.count }.by(1)
 
       # 送信した値がブラウザに表示されていることを確認する
+      expect(page).to have_content(post)
 
       # 送信した画像がブラウザに表示されていることを確認する
-
+      expect(page).to have_selector('img')
     end
-  end
+end
