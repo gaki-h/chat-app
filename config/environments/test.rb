dbabb2942b154cec7741abf5083f1d023ea5bbdc
gaki-h <walter.thi.gks@gmail.com>
2022-01-12 16:45:10 +0900

    ログイン遷移

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,6 +45,6 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-end
+  config.active_job.queue_adapter = :inline #追記 これはdo~endの中に追記する
 
-config.active_job.queue_adapter = :inline #追記
+end
